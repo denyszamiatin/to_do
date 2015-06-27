a91d0155b36b6f35da91f9c8502b69f807532533
Illesss <Konovalovillias@gmail.com>
2015-06-27 13:30:35 +0300

    добавляет в глобальную переменную дату и текст напоминания в виде списка

---
+++ add.py
@@ -0,0 +1,11 @@
+__author__ = 'illes'
+name =  __import__("Create a to_do list")
+""" adding date and text in global value task
+"""
+
+def add(date, text):
+    name.task.append([date, text])
+
+
+
+
