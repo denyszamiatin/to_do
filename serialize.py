c04a04fc5c941da3ade796cd6b069d33104fca32
Trion <sambdima@gmail.com>
2015-06-27 16:04:03 +0300

    Cleare ide settings. Added documentation and doctest to module serialize.py

--- serialize.py
+++ serialize.py
@@ -1,14 +1,32 @@
 import pickle
 
+
 def list_serialize(data, file_name):
+    """Return boolean result of writing data to file
+
+    Keyword arguments:
+    data - list of info to write
+    file_name - string path to file
+    
+    >>> list_serialize([], 'test.txt')
+    True
+    >>> list_serialize((1,2,3), 'test.txt')
+    False
+    """
     if type(data) is list:
-        pickle.dump(data,open(file_name,'w'))
+        pickle.dump(data, open(file_name, 'w'))
         return True
     else:
         return False
 
+
 def list_deserialize(file_name):
+    """Return unserialized data from file
+    
+    Keyword arguments:
+    file_name - string path to file
+    """
     try:
-        return pickle.load(open(file_name,'r'))
+        return pickle.load(open(file_name, 'r'))
     except IOError:
         return None
