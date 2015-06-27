042023fdd5e17505e84f4b69debd7ac7abd9e181
Trion <sambdima@gmail.com>
2015-06-27 12:45:07 +0300

    Task 7. DeSerialize data from file in: file_name as Name of file in current directory out: data from file or None, if file not exists

--- serialize.py
+++ serialize.py
@@ -6,3 +6,9 @@ def list_serialize(data, file_name):
         return True
     else:
         return False
+
+def list_deserialize(file_name):
+    try:
+        return pickle.load(open(file_name,'r'))
+    except IOError:
+        return None
