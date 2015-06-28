de7d6d8e588157c847e28204fa8a508aa2c4e1de
SJulia <sambjulia@jmail.com>
2015-06-28 20:46:06 +0300

    Update add.py
    Добавлена валидация даты

--- add.py
+++ add.py
@@ -1,12 +1,19 @@
 __author__ = 'illes'
+from datetime import datetime, date
 from create_list1 import task
 
-""" adding date and text in global value task
 """
+adding date and text in global value task
+"""
+def date_validate(my_date):
+    try:
+        day, month, year = my_date.split('.')
+        d = date(int(year), int(month), int(day))
+        res = d.strftime('%d.%m.%Y')
+        print res
+        return res
+    except ValueError:
+        raise Exception('Date was setted with errors')
 
 def add(date, text):
-    task.append([date, text])
-
-
-
-
+    task.append([date_validate(date), text])
