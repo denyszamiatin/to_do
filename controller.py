68645fe3db79f7a5bf9f6f617457454be7410afa
Imelstorm <admin@imelstorm.com.ua>
2015-06-30 23:53:03 +0300

    Task #14

---
+++ controller.py
@@ -0,0 +1,20 @@
+from main import *
+
+def menu():
+    print "This is Todo List. Please input your choice."
+
+    while True:
+        print """
+        1. Add Task
+        2. Exit
+        """
+        choice = raw_input('Your choice: ')
+        if choice == '1':
+            add_task(fail_if_invalid_date(input_date()), input_description())
+        elif choice == '2':
+            exit()
+        else:
+            print 'Wrong choice try again'
+            continue
+
+menu()
