5a1c65ff52b17649c2110eab190f0544e2fa61c5
viktoria <viktoriazagorodnia@gmail.com>
2015-06-30 23:01:10 +0300

    Done: Список задач для даты #9

--- main.py
+++ main.py
@@ -16,7 +16,6 @@ def fail_if_invalid_date(my_date):
     except ValueError:
         raise Exception('Date was setted with errors')
 
-
 def add_task(date, description):
     if date in tasks:
         tasks[date].append(description)
@@ -27,5 +26,11 @@ def return_today_tasks():
     today = datetime.today().strftime('%d.%m.%Y');
     if today in tasks:
         return tasks[today]
+    else:
+        return None
+
+def all_tasks_on_date(date):
+    if date in tasks:
+        return tasks[date]
     else:
         return None
