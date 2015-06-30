c061e4b23369ba6ff852ce042801a3b019bc5e64
Trion <sambdima@gmail.com>
2015-06-30 23:31:38 +0300

    Task #12

--- main.py
+++ main.py
@@ -23,14 +23,24 @@ def add_task(date, description):
     else:
         tasks[date] = [description]
 
+
+def all_tasks_on_date(date):
+    if date in tasks:
+        return tasks[date]
+    else:
+        return None 
+
+
 def return_today_tasks():
     today = datetime.today().strftime('%d.%m.%Y');
-    if today in tasks:
-        return tasks[today]
-    else:
-        return None
+    return all_tasks_on_date(today)    
 
 
 def del_task_by_date(date, index):
     if date in tasks and len(tasks[date]) - 1 >= index:
         tasks[date].pop(index)
+
+
+def modify_task_by_date_and_index(date, index, description):
+    if date in tasks and len(tasks[date]) - 1 >= index:
+        tasks[date][index] = description
