fae8c92df2450d648faea3aa076e1c844c49fc8d
Imelstorm <admin@imelstorm.com.ua>
2015-07-03 22:58:48 +0300

    Task #19

--- create_web_app.py
+++ create_web_app.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 from flask import Flask, render_template, request
-from main import tasks, add_task
+from main import tasks, add_task, fail_if_invalid_date
 
 web_app = Flask(__name__)
 
@@ -17,8 +17,8 @@ def addtask():
     error_text = ''
     try:
         if request.method == 'POST':
-            year, month, day  = request.form['date'].split('-')
-            add_task('{0}.{1}.{2}'.format(day, month, year), request.form['description'])
+            year, month, day = request.form['date'].split('-')
+            add_task(fail_if_invalid_date('{0}.{1}.{2}'.format(day, month, year)), request.form['description'])
             successful_text = u'Данные сохранены'
     except Exception as e:
         error_text = e.message
